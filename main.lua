ab26792d16053f4bfeda083a61eed458c1698949
Koil <qwost9513@gmail.com>
2024-11-14 14:31:58 +0900

    finding how to show console

--- main.lua
+++ main.lua
@@ -1,6 +1,4 @@
--- main.lua
-function love.draw()
-    --화면상에 텍스트 출력
-    --love.graphics.print("출력할 텍스트", x좌표, y좌표)
-    love.graphics.print("Hello World", 400, 300)
-end
+io.stdout:setvbuf("no")
+--main.lua
+print(3+4)
+
